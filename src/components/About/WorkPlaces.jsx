59ed3c5f60426e327e2fa0d4a270dd1a1eeff5b6
Jared Nielson <jarednielson@Jareds-MacBook-Pro-2.local>
2024-02-23 19:24:58 -0700

    Fix alt tags of workplace images

--- src/components/About/WorkPlaces.jsx
+++ src/components/About/WorkPlaces.jsx
@@ -8,16 +8,16 @@ function WorkPlaces() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="card__icons">
-        <img src={INL} className="img-fluid logo" alt="brand" />
+        <img src={INL} className="img-fluid" alt="INL-Logo" />
       </Col>
       <Col xs={4} md={2} className="card__icons">
-        <img src={Bowlero} className="img-fluid logo" alt="brand" />
+        <img src={Bowlero} className="img-fluid" alt="Bowlero-Logo" />
       </Col>
       <Col xs={4} md={2} className="card__icons">
-        <img src={Kyani} className="img-fluid logo" alt="brand" />
+        <img src={Kyani} className="img-fluid" alt="Kyani-Logo" />
       </Col>
       <Col xs={4} md={2} className="card__icons">
-        <img src={GM} className="img-fluid logo" alt="brand" />
+        <img src={GM} className="img-fluid" alt="GM-Logo" />
       </Col>
     </Row>
   );
