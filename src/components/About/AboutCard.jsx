652c6cba5e15c2cf17d5134ae0aff5dea1bdf2c7
Jared Nielson <jarednielson@Jareds-MacBook-Pro-2.local>
2024-08-12 21:33:49 -0600

    Fix wording in about

--- src/components/About/AboutCard.jsx
+++ src/components/About/AboutCard.jsx
@@ -6,9 +6,9 @@ function AboutCard() {
       <Card.Body>
         <blockquote className='blockquote mb-0'>
           <p className='about-text'>
-            I&apos;m a family-oriented individual, blessed with a supportive wife who
-            constantly inspires me to strive for excellence. Together, we
-            cherish the joy and adventure brought by our three adorable
+            I&apos;m a family-oriented individual, blessed with a supportive
+            wife who constantly inspires me to strive for excellence. Together,
+            we cherish the joy and adventure brought by our three adorable
             children, who undoubtedly light up our lives and keep each day
             filled with excitement.
             <br />
@@ -17,11 +17,11 @@ function AboutCard() {
             Laboratory.
             <br />
             <br />
-            In my downtime, I&apos;m often enjoying time with my family. I also
-            have a passion for activities such as playing
-            pickleball, hitting the golf course, or flying RC planes. Exploring
-            new destinations through travel, delving into DIY projects, and
-            immersing myself in technologies.
+            In my downtime, I&apos;m often enjoying time with my family. I have
+            a passion for activities such as playing pickleball, golfing, and
+            flying RC air planes. I also love exploring new destinations through
+            travel, delving into DIY projects, and immersing myself in
+            technologies.
           </p>
         </blockquote>
       </Card.Body>
